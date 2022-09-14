07dfd3cd00a14d798b27a08657d2e95f2fa7b158
MatthewPrashker <prashker.m@northeastern.edu>
2022-09-14 19:34:42 -0400

    started structs

--- learned-file-system/src/lib.rs
+++ learned-file-system/src/lib.rs
@@ -1,7 +1,29 @@
-use fuse::Filesystem;
+use fuse::{Filesystem, mount};
 use std::os::raw::c_int;
 
-pub struct LearnedFileSystem {}
+pub struct LearnedFileSystem {
+    FS_BLOCK_SIZE: u32,
+    FS_MAGIC: u32,
+    SuperBlock: FsSuperBlock,
+}
+
+pub struct FsSuperBlock {
+    magic: u32,
+    disk_size: u32,
+    padding: Vec<u8>,
+}
+
+pub struct FSINode {
+    uid: u16,
+    gid: u16,
+    mode: u32,
+    ctime: u32,
+    mtime: u32,
+    size: u32,
+    pointers: Vec<usize>,
+}
+
+
 
 impl Filesystem for LearnedFileSystem {
     fn init(&mut self, _req: &fuse::Request) -> Result<(), c_int> {
