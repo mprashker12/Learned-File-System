89f62e6465e1cd18de93276b660df6038318d483
MatthewPrashker <prashker.m@northeastern.edu>
2022-09-15 09:23:54 -0400

    utils

--- learned-file-system/src/lib.rs
+++ learned-file-system/src/lib.rs
@@ -1,13 +1,20 @@
 use fuse::{Filesystem};
 use std::os::raw::c_int;
+use std::io::{Read, Write};
+
+mod utils;
 
 pub struct LearnedFileSystem {
+    /// file descriptor for the disk
+    disk_fd: usize,
+    /// how many bytes in each block?
     fs_block_size: u32,
     fs_magic: u32,
     super_block: FsSuperBlock,
+    bit_mask_block: BitMaskBlock,
 }
 
-/// Block of the file system with inumber 2
+/// Block of the file system with inumber 0
 /// Records meta-data about the entire file system
 pub struct FsSuperBlock {
     magic: u32,
@@ -35,7 +42,7 @@ pub struct FSINode {
 
 impl Filesystem for LearnedFileSystem {
     fn init(&mut self, _req: &fuse::Request) -> Result<(), c_int> {
-       
+        
         Ok(())
     }
 
